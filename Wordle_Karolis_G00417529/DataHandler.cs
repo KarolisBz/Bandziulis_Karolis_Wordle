ab83cf8c4403ef7acb7a62b120a715e2c53f06bd
KarolisBz <25.spell@gmail.com>
2023-12-23 17:47:24 +0000

    Created DataHandler, testing github

--- Wordle_Karolis_G00417529/DataHandler.cs
+++ Wordle_Karolis_G00417529/DataHandler.cs
@@ -1,6 +1,21 @@
 ﻿namespace Wordle_Karolis_G00417529
 {
+    // this class handles most of loading and saving of data
     public class DataHandler
     {
+        // class fields //
+        // player data
+        static String currentPlayer;
+        // settings data
+        static bool darkMode;
+        static float fontSize;
+        static bool easyMode;
+        static bool timerOn;
+
+        // constructor
+        public void main()
+        {
+
+        }
     }
 }
